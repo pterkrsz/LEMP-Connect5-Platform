d45469eb259e041b6af8fd1126a62e5fc358e3a5
pterkrsz <72089970+pterkrsz@users.noreply.github.com>
2025-07-16 14:34:44 +0200

    Simplify project to InfluxDB connection

--- LEMP.Application/Interfaces/ITwoFactorService.cs
+++
@@ -1,7 +0,0 @@
-namespace LEMP.Application.Interfaces;
-
-public interface ITwoFactorService
-{
-    Task<string?> GetSecretAsync(string username);
-    Task SetSecretAsync(string username, string secret);
-}
