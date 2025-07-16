011db98af07d8522fdd9fd1e64d06944579a57fb
pterkrsz <72089970+pterkrsz@users.noreply.github.com>
2025-07-16 17:17:26 +0200

    Switch to InfluxDB3.Client

--- LEMP.Application/Interfaces/IMeasurementService.cs
+++
@@ -1,9 +0,0 @@
-using LEMP.Application.DTOs;
-
-namespace LEMP.Application.Interfaces;
-
-public interface IMeasurementService
-{
-    Task AddMeasurementAsync(MeasurementDto dto);
-    Task<IEnumerable<MeasurementDto>> GetAllAsync();
-}
